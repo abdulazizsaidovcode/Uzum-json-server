91a527ff1bc9f6b537b5f5c039c7c680355fb035
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-02-26 18:00:17 +0500

    cahnge some eamy

--- js/script.js
+++
@@ -1,9 +0,0 @@
-fetch("http://localhost:3000/category/1")
-    .then(res => {
-        return res.json()
-    })
-    .then(ress => {
-        console.log(ress.categarysType);
-    }).catch((err) => {
-        console.error(err)
-    })
